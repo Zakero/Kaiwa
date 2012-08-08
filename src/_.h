6e7bfb762a862277e620537ef7661acb156bebc8
Zakero <zakero@gmail.com>
2012-08-08 04:46:36 -0500

    Created source code template files.
    To help provide a consistant commenting pattern across all source
    files, the following templates have been created: _.cpp and _.h.

---
+++ src/_.h
@@ -0,0 +1,62 @@
+/******************************************************************************
+ * Copyright 2012 Andrew Moore
+ *
+ * This file is part of Kaiwa.
+ *
+ * Kaiwa is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Kaiwa is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Kaiwa.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef XXX_H
+#define XXX_H
+
+/******************************************************************************
+ * Defines
+ */
+
+
+/******************************************************************************
+ * Includes
+ */
+//ANSI/POSIX
+
+// Qt
+
+// Local
+
+
+/******************************************************************************
+ * Namespaces
+ */
+
+
+/******************************************************************************
+ * Forward Declarations
+ */
+
+
+/******************************************************************************
+ * Typedefs
+ */
+
+
+/******************************************************************************
+ * Structures
+ */
+
+
+/******************************************************************************
+ * Classes
+ */
+
+#endif
