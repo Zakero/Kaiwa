4d531aa1891709e98119bb16f7c706c2b03b4265
Zakero <zakero@gmail.com>
2012-08-17 02:15:38 -0500

    Restructured the echo's and added Network support.
     - Instead of using a sequence of echo's, use a single "heredoc" to
    update the Project file.  This allows for easier maintenance.
     - Added "Network" to the list of required QT modules.

--- setup.sh
+++ setup.sh
@@ -5,12 +5,19 @@ then
 fi
 
 qmake -project -o Kaiwa.pro
-echo "" >> Kaiwa.pro
-echo "# Custom Configuration from setup.sh" >> Kaiwa.pro
-echo "unix:DESTDIR = ." >> Kaiwa.pro
-echo "unix:OBJECTS_DIR = .obj " >> Kaiwa.pro
-echo "unix:MOC_DIR = .moc " >> Kaiwa.pro
-echo "unix:RCC_DIR = .rcc " >> Kaiwa.pro
-echo "unix:UI_DIR = .ui " >> Kaiwa.pro
+
+cat <<- EOL >> Kaiwa.pro
+
+	# Custom Configuration from setup.sh
+	unix:DESTDIR = .
+	unix:OBJECTS_DIR = .obj
+	unix:MOC_DIR = .moc
+	unix:RCC_DIR = .rcc
+	unix:UI_DIR = .ui
+	
+	CONFIG += qt
+	QT += network
+
+EOL
 
 qmake Kaiwa.pro
